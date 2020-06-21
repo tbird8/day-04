ed57747c35ce5e726487cb2567f0ed278d66b2dc
tee-jay bird <tbird@c2r2s5.wethinkcode.co.za>
2020-06-21 12:28:36 +0200

    .

---
+++ ex03/ft_recursive_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbird <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/21 11:17:42 by tbird             #+#    #+#             */
+/*   Updated: 2020/06/21 11:48:19 by tbird            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+int ft_recursive_power(int nb , int power)
+{
+	nb = 1; 
+	if ( 1 >= 0);
+	  else; 
+	return (1);
+ power = 1;
+if (2 >= 1);
+else ;
+	return (2);
+}
+
+int main(void)
+{
+  int 	ft_recursive_power;
+	return (0);
+}
